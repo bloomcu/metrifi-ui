ed9a236425504e7d5edd346072c4c07fc10353d9
Ryan Harmon <ryan@bloomcu.com>
2024-01-16 13:48:21 -0700

    Initial apex charts setup

--- src/views/funnels/Funnel.vue
+++ src/views/funnels/Funnel.vue
@@ -1,7 +1,7 @@
 <template>
   <LayoutDefault width="full">
     <div class="flex flex-row gap-12">
-      <aside class="basis-48 pr-6 border-r flex flex-col gap-6">
+      <aside class="basis-72 pr-6 border-r flex flex-col gap-6">
         Steps
       </aside>
 
@@ -22,6 +22,8 @@
             <span class="absolute bottom-0 text-xs font-bold">Feb</span>
           </div>
         </div> -->
+
+        <ApexChart type="bar" :options="chart.options" :series="chart.series"></ApexChart>
       </main>
     </div>
 
@@ -34,9 +36,49 @@ import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { funnelApi } from '@/domain/funnels/api/funnelApi.js'
 import LayoutDefault from '@/app/layouts/LayoutDefault.vue'
+import ApexChart from "vue3-apexcharts";
 
 const route = useRoute()
 const funnel = ref()
+
+const chart = ref({
+  options: {
+    colors: ['#4f46e5'],
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "70%",
+        borderRadiusApplication: "end",
+        borderRadius: 12,
+      },
+    },
+    chart: {
+      id: 'funnel',
+      animations: {
+        enabled: true,
+        easing: 'easein',
+        speed: 600,
+        animateGradually: {
+            enabled: true,
+            delay: 150
+        },
+        dynamicAnimation: {
+            enabled: false,
+            speed: 350
+        }
+      },
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993]
+    }
+  },
+  series: [
+    {
+      name: 'steps',
+      data: [30, 40, 45]
+    }
+  ]
+})
 
 function storeFunnelStep(step) {
   // funnelApi.storeStep(route.params.funnel, step).then(response => {
