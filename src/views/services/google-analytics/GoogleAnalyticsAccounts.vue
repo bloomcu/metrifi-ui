150eb827c0f1776057086c8c556a66a6ec471195
Ryan Harmon <ryan@bloomcu.com>
2024-01-23 15:49:56 -0700

    Use account name on connections

--- src/views/services/google-analytics/GoogleAnalyticsAccounts.vue
+++ src/views/services/google-analytics/GoogleAnalyticsAccounts.vue
@@ -1,5 +1,5 @@
 <template>
-  <LayoutDefault>
+  <LayoutDefault width="xs">
     <!-- Header -->
     <AppHeader v-if="accounts" class="pt-6">
       <h1 class="text-3xl font-medium leading-6 text-gray-900">Accounts</h1>
@@ -13,13 +13,16 @@
       <ul role="list" class="divide-y divide-gray-100">
         <template v-for="account in accounts" :key="account.account">
           <li v-if="account.propertySummaries" v-for="property in account.propertySummaries" :key="property.property" class="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6 cursor-pointer">
-            <div class="min-w-0 flex-auto">
-              <p class="mb-1 text-sm leading-5 text-gray-400">{{ account.displayName }}</p>
-              <p class="text-base font-medium leading-6 text-gray-900">{{ property.displayName }}</p>
+            <div class="min-w-0 flex items-center gap-6">
+              <svg class="w-6 h-6" viewBox="-14 0 284 284" preserveAspectRatio="xMidYMid"><path d="M256.003 247.933a35.224 35.224 0 0 1-39.376 35.161c-18.044-2.67-31.266-18.371-30.826-36.606V36.845C185.365 18.591 198.62 2.881 216.687.24A35.221 35.221 0 0 1 256.003 35.4v212.533Z" fill="#F9AB00"/><path d="M35.101 213.193c19.386 0 35.101 15.716 35.101 35.101 0 19.386-15.715 35.101-35.101 35.101S0 267.68 0 248.295c0-19.386 15.715-35.102 35.101-35.102Zm92.358-106.387c-19.477 1.068-34.59 17.406-34.137 36.908v94.285c0 25.588 11.259 41.122 27.755 44.433a35.161 35.161 0 0 0 42.146-34.56V142.089a35.222 35.222 0 0 0-35.764-35.282Z" fill="#E37400"/></svg>
+              <div class="flex-auto">
+                <p class="mb-1 text-sm leading-5 text-gray-400">{{ account.displayName }}</p>
+                <p class="text-base font-medium leading-6 text-gray-900">{{ property.displayName }}</p>
+              </div>
             </div>
 
             <div class="flex items-center gap-x-4 z-10">
-              <AppButton @click="storeConnection(property)" variant="tertiary">Connect</AppButton>
+              <AppButton @click="storeConnection(account.displayName, property)" variant="tertiary">Connect</AppButton>
             </div>
           </li>
         </template>
@@ -62,10 +65,11 @@ function listAccounts() {
   })
 }
 
-function storeConnection(property) {
+function storeConnection(accountName, property) {
   // TODO: Get organization from authStore
   connectionApi.store('bloomcu', {
     service: 'Google Analytics - Property',
+    account_name: accountName,
     name: property.displayName,
     uid: property.property,
     token: token.value
