5c14217878bb0de61ef4c5f07a2055e59abb9be1
Ryan Harmon <ryan@bloomcu.com>
2024-01-23 11:35:44 -0700

    Start report export functionality

--- src/domain/services/google-analytics/api/gaDataApi.js
+++ src/domain/services/google-analytics/api/gaDataApi.js
@@ -9,6 +9,15 @@ const gaDataApi = {
   async runReport(connectionId, request) {
     return await HttpClient.post(`/ga/report/${connectionId}`, request)
   },
+
+  // /**
+  //  * Export a Google Analytics report.
+  //  *
+  //  * @return promise
+  //  */
+  // async exportReport(connectionId, request) {
+  //   return await HttpClient.post(`/ga/export/${connectionId}`, request)
+  // },
 }
 
 export { gaDataApi }
