17f943cf07a25745b6e13a3d4b87df1f80e2fdfe
Ryan Harmon <ryan@bloomcu.com>
2024-01-30 13:35:33 -0700

    Stub in code for percentage based bar height.

--- src/views/funnels/components/chart/ChartBar.vue
+++ src/views/funnels/components/chart/ChartBar.vue
@@ -1,18 +1,34 @@
 <template>
-    <div class="bg-indigo-600 flex-1 h-full origin-bottom transition duration-300" />
+  <!-- <div :style="`height: ${height}%;`" class="bg-indigo-600 flex-1 rounded-lg" /> -->
+  <div class="bg-indigo-600 flex-1 h-full origin-bottom transition duration-300" />
 </template>
 
 <script setup>
+// import { computed } from 'vue'
+
 const props = defineProps({
+  // index: {
+  //   type: Number,
+  // },
   height: {
     type: Number,
   },
+  // value: {
+  //   type: Number,
+  // },
+  // max: {
+  //   type: Number,
+  // },
 })
+
+// const height = computed(() => {
+//   if (props.index === 0) return 100
+//   return props.value / props.max * 100
+// })
 </script>
 
 <style lang="scss">
-// --height: v-bind(height);
 div {
-    transform: scaleY(v-bind(height));
+  transform: scaleY(v-bind(height));
 }
 </style>
