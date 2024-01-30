619496c9e0e3f21757e4492fde197abb9c02c698
Ryan Harmon <ryan@bloomcu.com>
2024-01-30 00:08:25 -0700

    Funnel MVP working

--- src/views/funnels/Funnel.vue
+++ src/views/funnels/Funnel.vue
@@ -1,12 +1,16 @@
 <template>
   <LayoutDefault v-if="funnel"  width="full" class="min-h-screen flex flex-col">
-    <header class="border-b p-2">Header</header>
+    <header class="border-b p-3 flex items-center justify-between">
+      Header
+      <AppButton @click="runReport()">Run Report</AppButton>
+    </header>
 
     <div class="flex-1 flex flex-col sm:flex-row">
+
       <!-- Funnel steps -->
-      <nav class="w-72 border-r">
+      <nav class="w-[25rem] border-r">
         <!-- Header -->
-        <div class="flex items-center justify-between border-b p-2">
+        <div class="flex items-center justify-between border-b p-3">
           <p>Funnel steps</p>  
           <button @click="addStep()" type="button" class="group inline-flex items-center rounded-md p-1 bg-indigo-600 hover:bg-indigo-500 active:translate-y-px">
             <PlusIcon class="h-5 w-5 shrink-0 text-white" />
@@ -14,9 +18,10 @@
         </div>
 
         <!-- Steps -->
-        <div v-if="funnel.steps.length" class="flex flex-col gap-2 p-2">
-          <div v-for="step in funnel.steps" @click="activeStepId = step.id" class="group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-medium cursor-pointer bg-gray-50 text-gray-700 hover:text-indigo-600 hover:bg-gray-100">
-            <Bars2Icon class="h-5 w-5 shrink-0 text-gray-400 group-hover:text-indigo-600" />
+        <div v-if="funnel.steps.length" class="flex flex-col gap-3 p-3">
+          <div v-for="step in funnel.steps" @click="activeStepId = step.id" class="group flex items-center gap-x-2 rounded-lg px-2 py-3 text-sm leading-6 font-medium cursor-pointer text-gray-700 bg-white border hover:text-indigo-600 hover:bg-indigo-50">
+            <Bars2Icon class="h-4 w-4 shrink-0 cursor-grab text-gray-400 group-hover:text-indigo-600" />
+            <span class="inline-flex items-center rounded-md bg-indigo-100 px-2 py-1 text-xs font-medium text-indigo-700">{{ step.order }}</span>
             <p>{{ step.name }}</p>
           </div>
         </div>
@@ -30,79 +35,70 @@
       </nav>
 
       <!-- Active Step -->
-      <aside v-if="activeStep" class="w-96 border-r">
+      <aside v-if="activeStep" class="w-[25rem] border-r">
         <!-- Header -->
-        <div class="flex items-center justify-between border-b p-2">
+        <div class="flex items-center justify-between border-b p-3">
           <p>{{ activeStep.name }}</p>  
           <button @click="activeStepId = null" type="button" class="group inline-flex items-center rounded-md p-1 bg-gray-50 hover:bg-gray-100 active:translate-y-px">
             <XMarkIcon class="h-5 w-5 shrink-0 text-gray-400 group-hover:text-indigo-600" />
           </button>
         </div>
 
-        <!-- Step -->
-        <div class="border-b p-2">
+        <!-- Description -->
+        <!-- <div class="border-b p-2">
           <p class="text-sm leading-5 text-gray-400">{{ activeStep.description }}</p>
-        </div>
-        <div class="flex flex-col gap-3 p-2">
-          <AppSelect 
-            v-model="activeStep.expression.type" 
-            label="Type" 
-            :options="[
-              {label: 'View', value: 'view'},
-              {label: 'Event', value: 'event'},
-              {label: 'outbound', value: 'outbound'},
-            ]"
-          />
+        </div> -->
 
-          <AppSelect 
-            v-model="activeStep.expression.field_name" 
-            label="Where" 
-            :options="[
-              {label: 'Page Path', value: 'pagePath'},
-              {label: 'Page Referrer', value: 'pageReferrer'},
-              {label: 'Landing Page', value: 'landingPage'},
-              {label: 'Outbound Link Url', value: 'linkUrl'},
-            ]"
-          />
-          
-          <AppSelect 
-            v-model="activeStep.expression.field_operator" 
-            label="Operator" 
-            :options="[
-              {label: 'Exactly matches (=)', value: 'EXACT'},
-              {label: 'Contains', value: 'CONTAINS'},
-              {label: 'Begins with', value: 'BEGINS_WITH'},
-              {label: 'Ends with', value: 'ENDS_WITH'},
-            ]"
-          />
+        <!-- Options -->
+        <div class="flex flex-col gap-3 p-3">
+          <div>
+            <!-- <p class="block text-sm font-medium text-gray-900">Metric</p> -->
+            <AppSelect 
+              v-model="activeStep.metric" 
+              label="Metric"
+              :options="[
+                {label: 'Page views', value: 'pageViews'},
+                {label: 'Outbound link clicks', value: 'outboundLinkClick'},
+                {label: 'Element clicks', value: 'elementClick'},
+                {label: 'Form submissions', value: 'formSubmission'},
+              ]"
+            />
+          </div>
 
-          <!-- TODO: Use listbox for this -->
-          <AppInput v-model="activeStep.expression.field_value" label="Value" hint="E.g., /contact/"/>
+          <div>
+            <p class="block mb-1 text-sm font-medium text-gray-900">Measurables</p>
+            <AppInput v-model="activeStep.measurables[0]" class="mb-2" placeholder="Enter a page path..."/>
+            <AppInput v-model="activeStep.measurables[1]" class="mb-2" placeholder="Enter a page path..."/>
+            <AppInput v-model="activeStep.measurables[3]" class="mb-2" placeholder="Enter a page path..."/>
+          </div>
+
+          <!-- <pre>{{ activeStep }}</pre> -->
         </div>
       </aside>
 
       <!-- Chart -->
-      <div class="flex-1 p-2">
-        <!-- 
-          Vanilla bar chart
-          Source: https://codepen.io/robstinson/pen/ZEpKvjM?editors=1000 
-        -->
-        <div class="flex items-end flex-grow w-full mt-2 space-x-2 sm:space-x-3">
-          <div class="relative flex flex-col items-center flex-grow pb-5 group">
-            <span class="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">$37,500</span>
-            <div class="relative flex justify-center w-full h-16 bg-indigo-400"></div>
-            <span class="absolute bottom-0 text-xs font-bold">Jan</span>
+      <!-- <div> -->
+        <ApexChart v-if="chart" style="width: 1200px;" class="block" type="bar" :options="chart.options" :series="chart.series"></ApexChart>
+        <!-- Vanilla bar chart Source: https://codepen.io/robstinson/pen/ZEpKvjM?editors=1000 -->
+        <!-- <div class="flex-1 p-2">
+          <div class="flex items-end flex-grow w-full pt-6 space-x-2 sm:space-x-3">
+            <div v-for="step in funnel.steps" class="relative flex flex-col items-center flex-grow pb-5 group">
+              <span class="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">{{ step.total ? step.total : 'Loading...' }}</span>
+              <div 
+                style="height: 100%;"
+              class="relative flex justify-center w-full bg-indigo-600 rounded"></div>
+              <span class="absolute bottom-0 text-xs font-bold">{{ step.name }}</span>
+            </div>
+            <div class="relative flex flex-col items-center flex-grow pb-5 group">
+              <span class="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">$45,000</span>
+              <div style="height: 400px;" class="relative flex justify-center w-full h-20 bg-indigo-600 rounded"></div>
+              <span class="absolute bottom-0 text-xs font-bold">Feb</span>
+            </div>
           </div>
-          <div class="relative flex flex-col items-center flex-grow pb-5 group">
-            <span class="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">$45,000</span>
-            <div class="relative flex justify-center w-full h-20 bg-indigo-400"></div>
-            <span class="absolute bottom-0 text-xs font-bold">Feb</span>
-          </div>
-        </div>
+        </div> -->
 
-        <!-- <ApexChart class="block" type="bar" :options="chart.options" :series="chart.series"></ApexChart> -->
         <!-- <pre>{{ funnel }}</pre> -->
-      </div>
+      <!-- </div> -->
     </div>
 
     <!-- TODO: Add loading state -->
@@ -112,6 +108,7 @@
 <script setup>
 import { ref, computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import { gaDataApi } from '@/domain/services/google-analytics/api/gaDataApi.js'
 import { funnelApi } from '@/domain/funnels/api/funnelApi.js'
 import { Bars2Icon, QueueListIcon } from '@heroicons/vue/24/outline'
 import { PlusIcon, XMarkIcon } from '@heroicons/vue/24/solid'
@@ -119,63 +116,154 @@ import LayoutDefault from '@/app/layouts/LayoutDefault.vue'
 import ApexChart from "vue3-apexcharts";
 
 const route = useRoute()
-const funnel = ref()
+// const funnel = ref()
+// const report = ref()
 const activeStepId = ref(null)
 const activeStep = computed(() => funnel.value.steps.find(step => step.id === activeStepId.value))
 
-const chart = ref({
-  options: {
-    colors: ['#4f46e5'],
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: "70%",
-        borderRadiusApplication: "end",
-        borderRadius: 12,
-      },
-    },
-    chart: {
-      id: 'funnel',
-      animations: {
-        enabled: true,
-        easing: 'easein',
-        speed: 600,
-        animateGradually: {
-            enabled: true,
-            delay: 150
-        },
-        dynamicAnimation: {
-            enabled: false,
-            speed: 350
-        }
-      },
-    },
-    xaxis: {
-      categories: ['Homepage', 'Auto Loan', 'Application']
-    }
-  },
-  series: [
-    {
-      name: 'Steps',
-      data: [600, 321, 45]
-    }
-  ]
-})
+function runReport() {
+  funnel.value.steps.forEach((step) => {
+    gaDataApi.fetchPageViews(6, {
+      startDate: '2024-01-22',
+      endDate: 'yesterday',
+      pagePaths: step.measurables,
+    }).then(response => {
+      if (response.data.data.error) {
+        console.log(response.data.data.error)
+        return
+      }
+      
+      // console.log(response.data.data)
+      let report = response.data.data
+      step.total = report.totals[0].metricValues[0].value
 
-function addStep() {
-  funnelApi.storeStep(route.params.organization, route.params.funnel, {
-    name: 'New step',
-    description: 'This is the step description',
-  }).then(response => {
-    funnel.value.steps.push(response.data.data)
+      chart.value = {
+        options: {
+          colors: ['#4f46e5'],
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: "70%",
+              borderRadiusApplication: "end",
+              borderRadius: 12,
+            },
+          },
+          chart: {
+            id: 'funnel',
+            animations: {
+              enabled: true,
+              easing: 'easein',
+              speed: 600,
+              animateGradually: {
+                  enabled: true,
+                  delay: 150
+              },
+              dynamicAnimation: {
+                  enabled: false,
+                  speed: 350
+              }
+            },
+          },
+          xaxis: {
+            categories: funnel.value.steps.map(step => step.name),  
+          }
+        },
+        series: [
+          {
+            name: 'Page views',
+            data: funnel.value.steps.map(step => step.total)
+          }
+        ]
+      }
+    })
   })
 }
 
 onMounted(() => {
-  funnelApi.show(route.params.organization, route.params.funnel).then(response => {
-    funnel.value = response.data.data
-  })
+  
+
+  // funnelApi.show(route.params.organization, route.params.funnel).then(response => {
+  //   funnel.value = response.data.data
+  // })
 })
+
+const funnel = ref({
+  id: 1,
+  user: {
+    id: 1,
+    name: 'Ryan Harmon',
+    email: 'john@doe.com',
+    role: 'admin',
+    created_at: '2024-01-23T23:47:12.000000Z'
+  },
+  connection: null,
+  name: 'New funnel',
+  description: 'This is the funnel descriptions',
+  steps: [
+    // {
+    //   id: 1,
+    //   order: 1,
+    //   metric: 'pageViews',
+    //   name: 'Homepage',
+    //   description: null,
+    //   measurables: [
+    //     '/',
+    //   ]
+    // },
+    {
+      id: 2,
+      order: 2,
+      metric: 'pageViews',
+      name: 'Loans',
+      description: null,
+      measurables: [
+        '/personal/loans/',
+      ]
+    },
+    {
+      id: 3,
+      order: 3,
+      metric: 'pageViews',
+      name: 'Auto Loan',
+      description: null,
+      measurables: [
+        '/personal/loans/vehicle/auto-loans/',
+      ]
+    },
+    {
+      id: 4,
+      order: 4,
+      metric: 'pageViews',
+      name: 'Auto Loan Application',
+      description: null,
+      measurables: [
+        '/personal/loans/vehicle/auto-loans/application/'
+      ]
+    },
+  ]
+})
+
+function addStep() {
+  // funnelApi.storeStep(route.params.organization, route.params.funnel, {
+  //   metric: 'pageViews',
+  //   name: 'New step',
+  //   description: null,
+  // }).then(response => {
+  //   funnel.value.steps.push(response.data.data)
+  // })
+
+  funnel.value.steps.push({
+    metric: 'pageViews',
+    name: 'New step',
+    description: null,
+    measurables: [
+      ''
+    ]
+  })
+}
+
+const chart = ref()
+
 </script>
 
 <style lang="scss">
