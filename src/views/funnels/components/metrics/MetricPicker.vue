cdb571849d61de1df4c15022373288f5975b3c6b
Ryan Harmon <ryan@bloomcu.com>
2024-02-08 14:59:43 -0700

    Tweaks

--- src/views/funnels/components/metrics/MetricPicker.vue
+++ src/views/funnels/components/metrics/MetricPicker.vue
@@ -1,7 +1,7 @@
 <template>
     <AppDropdown class="w-56">
       <template #title>
-        {{ selected ? selected : 'Loading...' }} 
+        Metric: {{ selected ? selected : 'Loading...' }} 
         <ChevronDownIcon class="ml-auto h-4 w-4 text-gray-400"/>
       </template>
       <button 
