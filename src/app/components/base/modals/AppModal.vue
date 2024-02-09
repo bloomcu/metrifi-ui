48d035c8addb6c22bad1d32da94349a8c9e8d258
Ryan Harmon <ryan@bloomcu.com>
2024-02-08 19:05:56 -0700

    Add more sizes to the app modal

--- src/app/components/base/modals/AppModal.vue
+++ src/app/components/base/modals/AppModal.vue
@@ -42,8 +42,10 @@ function close() {
 const emit = defineEmits(['closed'])
 
 const sizeClasses = {
-  sm: 'sm:max-w-sm',
-  md: 'sm:max-w-md',
-  lg: 'sm:max-w-lg',
+  'sm': 'sm:max-w-sm',
+  'md': 'sm:max-w-md',
+  'lg': 'sm:max-w-lg',
+  'xl': 'sm:max-w-xl',
+  '2xl': 'sm:max-w-2xl',
 }
 </script>
