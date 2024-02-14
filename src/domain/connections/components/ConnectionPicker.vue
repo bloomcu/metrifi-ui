d4e58d3f011763b065340ba7021599c953efcd51
Ryan Harmon <ryan@bloomcu.com>
2024-02-13 22:16:23 -0700

    Add label to connection picker

--- src/domain/connections/components/ConnectionPicker.vue
+++ src/domain/connections/components/ConnectionPicker.vue
@@ -1,5 +1,7 @@
 <template>
-    <AppDropdown class="w-60">
+    <div class="flex flex-col gap-2">
+      <label v-if="showLabel" class="block text-sm font-medium text-gray-900">Connection</label>
+      <AppDropdown>
         <template #title>
           <div class="flex items-center">
             <svg class="w-5 h-5 mr-2" viewBox="-14 0 284 284" preserveAspectRatio="xMidYMid"><path d="M256.003 247.933a35.224 35.224 0 0 1-39.376 35.161c-18.044-2.67-31.266-18.371-30.826-36.606V36.845C185.365 18.591 198.62 2.881 216.687.24A35.221 35.221 0 0 1 256.003 35.4v212.533Z" fill="#F9AB00"/><path d="M35.101 213.193c19.386 0 35.101 15.716 35.101 35.101 0 19.386-15.715 35.101-35.101 35.101S0 267.68 0 248.295c0-19.386 15.715-35.102 35.101-35.102Zm92.358-106.387c-19.477 1.068-34.59 17.406-34.137 36.908v94.285c0 25.588 11.259 41.122 27.755 44.433a35.161 35.161 0 0 0 42.146-34.56V142.089a35.222 35.222 0 0 0-35.764-35.282Z" fill="#E37400"/></svg>
@@ -10,6 +12,7 @@
           {{ connection.name }}
         </button>
       </AppDropdown>
+    </div>
 </template>
 
 <script setup>
@@ -21,6 +24,10 @@ const props = defineProps({
   },
   selected: {
     type: Object,
+  },
+  showLabel: {
+    type: Boolean,
+    default: false,
   },
 })
 
