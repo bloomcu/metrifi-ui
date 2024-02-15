5b435a690490323f765c7ae431aa3938f37e7be7
Ryan Harmon <ryan@bloomcu.com>
2024-02-15 11:07:26 -0700

    Set connection on funnel

--- src/app/components/datepicker/DatePicker.vue
+++ src/app/components/datepicker/DatePicker.vue
@@ -4,6 +4,7 @@
         <CalendarIcon  class="mr-1.5 h-5 w-5" />
         {{ selectedDateRange.label }} 
         <span class="text-gray-400 font-normal ml-1">({{ moment(selectedDateRange.startDate).format('MMM DD, YYYY') }} - {{ selectedDateRange.endDate }})</span>
+        <ChevronDownIcon class="ml-1 h-4 w-4 text-gray-400"/>
       </template>
       <button 
         v-for="option in dateRangeOptions" 
@@ -20,6 +21,7 @@
 import moment from 'moment'
 import { useDatePicker } from '@/app/components/datepicker/useDatePicker'
 import { CalendarIcon } from '@heroicons/vue/24/outline'
+import { ChevronDownIcon } from '@heroicons/vue/24/solid'
 import AppDropdown from '@/app/components/dropdown/AppDropdown.vue'
 
 const { dateRangeOptions, selectedDateRange, selectDateRange } = useDatePicker()
