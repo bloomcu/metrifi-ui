9218d7e38e809d51d5e0538bd4df6a22d6561e21
Ryan Harmon <ryan@bloomcu.com>
2024-02-25 14:09:36 -0700

    Fix modal min height

--- src/app/components/base/modals/AppModal.vue
+++ src/app/components/base/modals/AppModal.vue
@@ -8,7 +8,7 @@
       <div class="fixed inset-0 z-10">
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            <DialogPanel :class="`${sizeClasses[size]}`" class="min-h-[90vh] max-h-[90vh] relative transform overflow-y-auto overflow-x-hidden rounded-lg bg-white px-4 py-5 text-left shadow-xl transition-all sm:my-8 w-full sm:p-6     scrollbar scrollbar-thumb-blue-600 scrollbar-thumb-rounded">
+            <DialogPanel :class="`${sizeClasses[size]}`" class="max-h-[90vh] relative transform overflow-y-auto overflow-x-hidden rounded-lg bg-white px-4 py-5 text-left shadow-xl transition-all sm:my-8 w-full sm:p-6     scrollbar scrollbar-thumb-blue-600 scrollbar-thumb-rounded">
               <slot/>
             </DialogPanel>
           </TransitionChild>
