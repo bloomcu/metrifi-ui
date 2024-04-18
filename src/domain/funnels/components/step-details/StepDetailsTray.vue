15a7a3e58c4b3934101422d676138289f2395b8a
Ryan Harmon <ryan@bloomcu.com>
2024-04-18 10:42:37 -0600

    Edit funnels from dashboard. Scroll step details tray.

--- src/domain/funnels/components/step-details/StepDetailsTray.vue
+++ src/domain/funnels/components/step-details/StepDetailsTray.vue
@@ -2,31 +2,32 @@
   <template v-if="isOpen">
     <!-- <div @click="toggleTray()" class="fixed inset-0 h-full w-full overflow-y-auto bg-slate-600 bg-opacity-20"></div> -->
 
-    <div class="fixed left-0 top-0 h-full w-96 p-8 transform rounded-r-3xl bg-white shadow-2xl ring-1 ring-gray-900/5">
+    <div class="fixed left-0 top-0 h-full overflow-y-scroll w-96 p-6 transform rounded-r-3xl bg-white shadow-2xl ring-1 ring-gray-900/5">
       <!-- Tray header -->
-      <div class="flex items-center justify-between">
+      <div class="flex items-center justify-between mb-4">
         <h3 class="text-xl font-semibold">Step details</h3>
         <svg @click="toggleTray()" class="w-6 h-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
         </svg>
       </div>
 
+      <div class="mb-4">
+        <p class="font-medium text-gray-900">Step name</p>
+        <p class="text-gray-500">{{ selectedStep.name }}</p>
+      </div>
+
+      <div>
+        <p class="mb-1 font-medium text-gray-900">Metrics</p>
+      </div>
+
       <!-- Tray content -->
       <div class="mt-4">
-        <div class="flex flex-col gap-6">
-          <div>
-            <p class="font-medium text-gray-900">Step name</p>
-            <p class="text-gray-500">{{ selectedStep.name }}</p>
-          </div>
-
-          <div>
-            <p class="mb-1 font-medium text-gray-900">Metrics</p>
-
-            <!-- Measurables -->
-            <template v-for="(metric, index) in selectedStep.metrics">
+        <div class="flex flex-col gap-4">
+          <!-- Measurables -->
+          <template v-for="(metric, index) in selectedStep.metrics">
               <!-- Page users metric -->
-              <div v-if="metric.metric === 'pageUsers'" class="flex flex-col gap-4 bg-gray-50 rounded-md p-4 mb-2">
-                <div class="border-b pb-4">
+              <div v-if="metric.metric === 'pageUsers'" class="flex flex-col gap-2 bg-gray-50 rounded-md p-2">
+                <div class="border-b pb-2">
                   <p class="font-medium text-gray-900">Metric</p>
                   <p class="text-gray-500">{{ metric.metric }}</p>
                 </div>
@@ -96,7 +97,6 @@
                 </div>
               </div>
             </template>
-          </div>
         </div>
 
       </div>
