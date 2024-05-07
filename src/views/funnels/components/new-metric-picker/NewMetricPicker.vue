ad8610faabc18f9428939054aee0277619b39cd9
Ryan Harmon <ryan@bloomcu.com>
2024-05-07 12:02:24 -0600

    Modify picker table columns

--- src/views/funnels/components/new-metric-picker/NewMetricPicker.vue
+++ src/views/funnels/components/new-metric-picker/NewMetricPicker.vue
@@ -39,17 +39,18 @@
                   <tr class="divide-x divide-gray-200">
                       <th v-for="header in reports[selectedTab.metric].dimensionHeaders" scope="col" class="py-3 px-3 text-left">
                         <div class="text-sm font-semibold text-gray-900">{{ dictionary[header.name].displayName ?? header.name }}</div>
-                        <!-- <div class="mt-0.5 text-xs italic font-normal text-gray-400">{{ header.name }}</div> -->
+                        <div class="mt-0.5 text-xs italic font-normal text-gray-400">{{ header.name }}</div>
                       </th>
                       <th v-for="header in reports[selectedTab.metric].metricHeaders" scope="col" class="py-3 px-3 text-left">
                         <div class="text-sm font-semibold text-gray-900">
                           {{ dictionary[header.name].displayName ?? header.name }}
                           ({{ reports[selectedTab.metric].totals[0].metricValues[0].value }})
                         </div>
-                        <!-- <div class="mt-0.5 text-xs italic font-normal text-gray-400">{{ header.name }}</div> -->
+                        <div class="mt-0.5 text-xs italic font-normal text-gray-400">{{ header.name }}</div>
                       </th>
                   </tr>
                 </thead>
+                
                 <tbody class="divide-y divide-gray-200">
                   <tr 
                     v-if="selectedTab.metric === 'pageUsers'" 
@@ -106,12 +107,12 @@
                     })" 
                     class="divide-x divide-gray-200 cursor-pointer hover:bg-gray-50"
                   >
-                      <td class="py-3 px-3 text-sm text-gray-500 max-w-0 overflow-hidden text-ellipsis whitespace-nowrap w-[8%]">{{ row.dimensionValues[0].value }}</td> <!-- Event name -->
+                      <td class="py-3 px-3 text-sm text-gray-500 whitespace-nowrap w-[8%]">{{ row.dimensionValues[0].value }}</td> <!-- Event name -->
                       <td  class="py-3 px-3 text-sm text-gray-500 break-all w-[30%]">{{ row.dimensionValues[1].value ? row.dimensionValues[1].value : '(not set)'}}</td> <!-- Page path -->
                       <td  class="py-3 px-3 text-sm text-gray-500 break-all w-[30%]">{{ row.dimensionValues[2].value ? row.dimensionValues[2].value : '(not set)'}}</td> <!-- Form destination -->
-                      <td  class="py-3 px-3 text-sm text-gray-500 max-w-0 overflow-hidden text-ellipsis whitespace-nowrap w-[10%]">{{ row.dimensionValues[3].value ? row.dimensionValues[3].value : '(not set)'}}</td> <!-- Form id -->
+                      <td  class="py-3 px-3 text-sm text-gray-500 break-all w-[14%]">{{ row.dimensionValues[3].value ? row.dimensionValues[3].value : '(not set)'}}</td> <!-- Form id -->
                       <td  class="py-3 px-3 text-sm text-gray-500 break-all w-[1%]">{{ row.dimensionValues[4].value ? row.dimensionValues[4].value : '(not set)'}}</td> <!-- Form length -->
-                      <td  class="py-3 px-3 text-sm text-gray-500 max-w-0 overflow-hidden text-ellipsis whitespace-nowrap w-[5%]">{{ row.dimensionValues[5].value ? row.dimensionValues[5].value : '(not set)'}}</td> <!-- Form submit text -->
+                      <td  class="py-3 px-3 text-sm text-gray-500 whitespace-nowrap w-[5%]">{{ row.dimensionValues[5].value ? row.dimensionValues[5].value : '(not set)'}}</td> <!-- Form submit text -->
                       <td class="py-3 px-3 text-sm font-medium text-gray-900 break-all w-[1%]">{{ row.metricValues[0].value }}</td> <!-- Users -->
                   </tr>
                 </tbody>
