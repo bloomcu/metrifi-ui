14b3239bfcceeee6a6a8149635523d9a46511b3d
Ryan Harmon <ryan@bloomcu.com>
2024-05-16 14:05:58 -0600

    Fix redirect loop

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,17 @@
 
 <script setup>
 import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import { useOrganizations } from '@/domain/organizations/composables/useOrganizations'
 
+const route = useRoute()
 const { showOrganization } = useOrganizations()
 
 onMounted(() => {
   console.log('Mounting organization')
-  showOrganization()  
+  if (route.params.organization) {
+    showOrganization()
+  }
 })
 </script>
 
