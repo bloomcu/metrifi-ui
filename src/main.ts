b99abcd9ea3eea35eb7f78ed7b6e982f77512956
Ryan Harmon <ryan@bloomcu.com>
2024-05-16 14:06:04 -0600

    Add env

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,7 @@ const app = createApp(App)
 
 Sentry.init({
   app,
+  environment: import.meta.env.VITE_ENV,
   dsn: "https://72b08d5732125a3cec895d73d58c96e8@o4507262580948992.ingest.us.sentry.io/4507262581145600",
   integrations: [
     Sentry.browserTracingIntegration(),
