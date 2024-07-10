834141b066569cb1cc590e4bc5d71034c65a1fad
Ryan Harmon <ryan@bloomcu.com>
2024-07-09 18:30:27 -0600

    Minor updates

--- src/domain/analyses/store/useAnalysisStore.js
+++ src/domain/analyses/store/useAnalysisStore.js
@@ -31,7 +31,7 @@ export const useAnalysisStore = defineStore('analysisStore', {
 
           await AnalysisApi.store(organization, dashboard, params)
             .then(response => {
-              console.log(response.data.data)
+              console.log("Analysis: ", response.data.data)
               this.analysis = response.data.data
               this.isLoading = false
             }).catch(error => {
--- src/views/dashboards/DashboardBar.vue
+++ src/views/dashboards/DashboardBar.vue
@@ -44,7 +44,7 @@
         <!-- Run analysis -->
         <AppButton v-if="!analysisStore.analysis" @click="createAnalysis()" :loading="analysisStore.isLoading" variant="tertiary" size="base" class="flex items-center gap-2">
           <!-- <ChatBubbleBottomCenterIcon class="h-5 w-5 shrink-0" /> -->
-          Run analysis
+          Analyze with AI
         </AppButton>
 
         <!-- Zoom -->
@@ -109,7 +109,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
               <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
             </svg>
-            Re-run analysis
+            Re-analyze with AI
           </AppButton>
 
           <!-- Close analysis -->
@@ -139,7 +139,7 @@
 
     <div v-if="analysisStore.analysis && !isShowingAnalysis" @click="showAnalysis()" class="mb-4 px-6 py-4 border-2 border-gray-200 rounded-2xl bg-white cursor-pointer hover:bg-gray-50">
       <p class="text-xl font-medium leading-6 text-gray-900 tracking-tight">
-        Show analysis
+        Show AI analysis
         <span class="text-gray-400 text-sm font-normal">(Created on {{ moment(analysisStore.analysis.created_at).fromNow() }})</span>
       </p>
     </div>
