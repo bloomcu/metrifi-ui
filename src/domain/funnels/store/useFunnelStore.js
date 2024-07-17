d345a5350970fc0744107d6422e0df613012c9d8
Ryan Harmon <ryan@bloomcu.com>
2024-07-17 14:26:14 -0600

    Handle disabled steps in the backend

--- src/domain/funnels/store/useFunnelStore.js
+++ src/domain/funnels/store/useFunnelStore.js
@@ -95,67 +95,66 @@ export const useFunnelStore = defineStore('funnelStore', () => {
         // console.log('getReport...')
         isLoading.value = true
 
-        gaDataApi.funnelReport(funnel.connection_id, {
+        gaDataApi.funnelReport(funnel.id, {
+            disabledSteps: funnel.pivot?.disabled_steps,
             startDate: selectedDateRange.value.startDate,
             endDate: selectedDateRange.value.endDate,
-            steps: funnel.steps,
         }).then(response => {
             if (response.data.data.error) console.log(response.data.data.error)
-            funnel.report = response.data.data
-            removeDisabledStepsFromReport(funnel)
-            calculateReportConversions(funnel, funnel.report.steps)
+            funnel.report = response.data.data.report
+            // removeDisabledStepsFromReport(funnel)
+            // calculateReportConversions(funnel, funnel.report.steps)
             isLoading.value = false
         })
 
         startNextFunnelJob()
     }, 500)
 
-    function removeDisabledStepsFromReport(funnel) {
-        if (!funnel.pivot) return
+    // function removeDisabledStepsFromReport(funnel) {
+    //     if (!funnel.pivot) return
 
-        let disabled_steps = funnel.pivot.disabled_steps
-        if (!disabled_steps) return
+    //     let disabled_steps = funnel.pivot.disabled_steps
+    //     if (!disabled_steps) return
 
-        funnel.steps.forEach((step) => {
-            // console.log(`Step ${step.id} disabled: ${disabled_steps.includes(step.id)}`)
-            if (disabled_steps.includes(step.id)) {
-                // Find the index of the step in the report by it's id
-                let index = funnel.report.steps.findIndex(s => s.id === step.id)
+    //     funnel.steps.forEach((step) => {
+    //         if (disabled_steps.includes(step.id)) {
+    //             // Find the index of the step in the report by it's id
+    //             let index = funnel.report.steps.findIndex(s => s.id === step.id)
 
-                // Remove the step from the report
-                funnel.report.steps.splice(index, 1)
-            }
-        })
-    }
+    //             // Remove the step from the report
+    //             funnel.report.steps.splice(index, 1)
+    //         }
+    //     })
+    // }
 
-    function calculateReportConversions(funnel, steps) {
-        // console.log('calculateReportConversions...')
+    // function calculateReportConversions(funnel, steps) {
+    //     // console.log('calculateReportConversions...')
 
-        steps.forEach((step, index) => {
-            // Update user count
-            funnel.report.steps[index].users = step.users
+    //     steps.forEach((step, index) => {
+    //         // Update user count
+    //         funnel.report.steps[index].users = step.users
 
-            // First conversion rate is always 100%
-            if (index === 0) {
-                funnel.report.steps[0].conversionRate = '100'
-                return
-            }
+    //         // First conversion rate is always 100%
+    //         if (index === 0) {
+    //             funnel.report.steps[0].conversionRate = '100'
+    //             return
+    //         }
 
-            let cr = (step.users / steps[index - 1]?.users)
+    //         let cr = (step.users / steps[index - 1]?.users)
 
-            if (cr === Infinity || isNaN(cr)) {
-                funnel.report.steps[index].conversionRate = '0.00'
-                return
-            }
+    //         if (cr === Infinity || isNaN(cr)) {
+    //             funnel.report.steps[index].conversionRate = '0.00'
+    //             return
+    //         }
 
-            let formatted = cr * 100 // Get a percentage
-            formatted = formatted.toFixed(2) // Round to 2 decimal places
-            formatted = formatted.substring(0, 4) // Trim to 2 decimal places
+    //         let formatted = cr * 100 // Get a percentage
+    //         formatted = formatted.toFixed(2) // Round to 2 decimal places
+    //         formatted = formatted.substring(0, 4) // Truncate to 4 characters
 
-            // Update conversion rate
-            funnel.report.steps[index].conversionRate = formatted
-        })
-    }
+    //         // Update conversion rate
+    //         funnel.report.steps[index].conversionRate = formatted
+    //     })
+    // }
 
     return {
         funnels,
@@ -170,7 +169,7 @@ export const useFunnelStore = defineStore('funnelStore', () => {
         update,
         addFunnel,
         addFunnelJob,
-        calculateReportConversions,
+        // calculateReportConversions,
     }
 })
 
