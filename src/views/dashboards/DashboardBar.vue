1c57dfedfdfcf8b01ea70ca21317aac75b8b883a
Ryan Harmon <ryan@bloomcu.com>
2024-07-25 14:19:48 -0600

    Changes to term "AI" on analysis

--- src/views/dashboards/DashboardBar.vue
+++ src/views/dashboards/DashboardBar.vue
@@ -44,7 +44,7 @@
         <!-- Run analysis -->
         <AppButton v-if="!analysisStore.analysis" @click="storeAnalysis()" :loading="analysisStore.isLoading" variant="tertiary" size="base" class="flex items-center gap-2">
           <!-- <ChatBubbleBottomCenterIcon class="h-5 w-5 shrink-0" /> -->
-          Analyze with AI
+          Run analysis
         </AppButton>
 
         <!-- Zoom -->
@@ -98,7 +98,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
               <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
             </svg>
-            Re-analyze with AI
+            Rerun analysis
           </AppButton>
           
           <!-- Edit analysis -->
@@ -162,7 +162,7 @@
     </div>
 
     <div v-if="analysisStore.analysis && !isShowingAnalysis" @click="showAnalysis()" class="flex items-center gap-2 mb-4 px-6 py-4 border-2 border-gray-200 rounded-2xl bg-white cursor-pointer hover:bg-gray-50">
-      <p class="text-xl font-medium leading-6 text-gray-900 tracking-tight">Show AI analysis</p>
+      <p class="text-xl font-medium leading-6 text-gray-900 tracking-tight">Show analysis</p>
       <span class="text-gray-400 text-sm font-normal">Created on {{ moment(analysisStore.analysis.created_at).fromNow() }}</span>
     </div>
 
