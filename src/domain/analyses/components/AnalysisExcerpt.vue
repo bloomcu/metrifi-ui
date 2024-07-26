1e8ab2ba7ffd05f8dadd9df3b3a3ddf46c224487
Ryan Harmon <ryan@bloomcu.com>
2024-07-26 16:59:21 -0600

    Update analysis excerpt for all edge cases

--- src/domain/analyses/components/AnalysisExcerpt.vue
+++ src/domain/analyses/components/AnalysisExcerpt.vue
@@ -1,26 +1,32 @@
 <template>
-    <div v-if="analysis.subject_funnel_performance <= 0">
-        <p class="mb-3">
-            <span class="font-semibold">Conversion rate:</span>
-            {{ Number(analysis.subject_funnel_performance).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}% lower than the median of the comparisons
-        </p>
-        <p class="mb-3">
-            <span class="font-semibold">Biggest opportunity:</span>
-            Step {{ analysis.bofi_step_index + 1 }} of your funnel is {{ Number(analysis.bofi_performance).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}% {{ analysis.bofi_performance <= 0 ? 'lower' : 'higher' }} than the median of the comparisons
-        </p>
-        <p class="mb-4">
-            <span class="font-semibold">Potential assets: </span>
-            <span v-if="!analysis.subject_funnel_conversion_value">Assets not configured at time of last analysis</span>
-            <span v-else>+{{ Number(analysis.bofi_asset_change).toLocaleString('en-US', {style:'currency', currency:'USD', minimumFractionDigits: 0, maximumFractionDigits: 0}) }} every {{ analysis.period }} if you get Step {{ analysis.bofi_step_index + 1 }} on par with the median of the comparisons</span>
-        </p>
-    </div>
+    <!-- Overall conversion rate -->
+    <p v-if="analysis.subject_funnel_performance != 0" class="mb-3">
+        <span class="font-semibold">Conversion rate:</span>
+        {{ Number(analysis.subject_funnel_performance).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}% 
+        {{ analysis.subject_funnel_performance <= 0 ? 'lower' : 'higher' }} than the median of the comparisons
+    </p>
 
-    <div v-else>
-        <p class="mb-3">
-            <span class="font-semibold">Conversion rate:</span>
-            {{ Number(analysis.subject_funnel_performance).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}% higher than the median of the comparisons. Consider comparing it the highest performing funnels, or drive more traffic.
-        </p>
-    </div>
+    <!-- Biggest opportunity -->
+    <p v-if="analysis.bofi_performance < 0" class="mb-3">
+        <span class="font-semibold">Biggest opportunity:</span>
+        Step {{ analysis.bofi_step_index + 1 }} of your funnel is {{ Number(analysis.bofi_performance).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}% 
+        {{ analysis.bofi_performance <= 0 ? 'lower' : 'higher' }} than the median of the comparisons
+    </p>
+    <!-- Funnel is better on all steps, so the BOFI is to compare with higher performing funnels -->
+    <p v-if="analysis.bofi_performance >= 0" class="mb-3">
+        <span class="font-semibold">Biggest opportunity:</span>
+        Compare with higher performing funnels. Or, if you funnel is optimized, drive more traffic to it.
+    </p>
+
+    <!-- Potential assets -->
+    <p v-if="!analysis.subject_funnel_conversion_value" class="mb-4">
+        <span class="font-semibold">Potential assets: </span>
+        <span>Assets not configured at time of last analysis</span>
+    </p>
+    <p v-else-if="analysis.bofi_asset_change > 0" class="mb-4">
+        <span class="font-semibold">Potential assets: </span>
+        <span>{{ Number(analysis.bofi_asset_change).toLocaleString('en-US', {style:'currency', currency:'USD', minimumFractionDigits: 0, maximumFractionDigits: 0}) }} every {{ analysis.period }} if you get Step {{ analysis.bofi_step_index + 1 }} on par with the median of the comparisons</span>
+    </p>
 </template>
   
 <script setup>
