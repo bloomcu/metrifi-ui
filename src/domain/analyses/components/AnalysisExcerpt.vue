69836a89614fe1a8a4a890eafc65abdd414ce53c
Ryan Harmon <ryan@bloomcu.com>
2024-07-26 15:18:14 -0600

    Check if subject funnel had assets configured

--- src/domain/analyses/components/AnalysisExcerpt.vue
+++ src/domain/analyses/components/AnalysisExcerpt.vue
@@ -9,9 +9,9 @@
             Step {{ analysis.bofi_step_index + 1 }} of your funnel is {{ Number(analysis.bofi_performance).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}% {{ analysis.bofi_performance <= 0 ? 'lower' : 'higher' }} than comparisons
         </p>
         <p class="mb-4">
-            <span class="font-semibold">Potential assets:</span>
-            <!-- +{{ Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(analysis.bofi_asset_change) }} every {{ analysis.period }} if you get Step {{ analysis.bofi_step_index + 1 }} on par with comparisons -->
-            +{{ Number(analysis.bofi_asset_change).toLocaleString('en-US', {style:'currency', currency:'USD', minimumFractionDigits: 0, maximumFractionDigits: 0}) }} every {{ analysis.period }} if you get Step {{ analysis.bofi_step_index + 1 }} on par with comparisons
+            <span class="font-semibold">Potential assets: </span>
+            <span v-if="!analysis.subject_funnel_conversion_value">Assets not configured at time of last analysis</span>
+            <span v-else>+{{ Number(analysis.bofi_asset_change).toLocaleString('en-US', {style:'currency', currency:'USD', minimumFractionDigits: 0, maximumFractionDigits: 0}) }} every {{ analysis.period }} if you get Step {{ analysis.bofi_step_index + 1 }} on par with comparisons</span>
         </p>
     </div>
 
