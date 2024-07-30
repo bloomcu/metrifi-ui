9c9e08f510e1eb3a2849d912dccd17e065b01836
Ryan Harmon <ryan@bloomcu.com>
2024-07-30 14:20:10 -0600

    Add users count to dashboard card

--- src/views/dashboards/Dashboards.vue
+++ src/views/dashboards/Dashboards.vue
@@ -32,16 +32,21 @@
           class="group relative flex flex-col cursor-pointer overflow-hidden rounded-lg shadow-md border border-gray-200 bg-white hover:shadow-lg"
         >          
           <div class="flex flex-col space-y-4 px-4 py-4">
-            <!-- Card top -->
+            <!-- Card header -->
             <div :class="dashboard.latest_analysis ? 'border-b pb-4' : ''" class="flex items-center justify-between">
               <div class="flex items-center gap-2">
                 <ChartBarIcon class="w-5 text-indigo-600"/>
                 <h3 class="text-xl font-medium text-gray-900">{{ dashboard.name }}</h3>
               </div>
               
-              <div class="flex items-center gap-4">
-                <span class="text-gray-500">{{ dashboard.funnels.length }} funnels</span>
-              
+              <div class="flex items-center gap-3">
+                <!-- Users count -->
+                <span v-if="dashboard.latest_analysis && dashboard.latest_analysis.subject_funnel_users" class="text-sm text-gray-500 border-r border-gray-300 pr-3">{{ dashboard.latest_analysis.subject_funnel_users }} users</span>
+
+                <!-- Funnels count -->
+                <span class="text-sm text-gray-500 pr-3">{{ dashboard.funnels.length }} funnels</span>
+
+                <!-- Delete -->
                 <button @click.stop="destroyDashboard(dashboard.id)" class="cursor-pointer font-medium rounded-md p-1.5 text-sm text-gray-400 bg-white hover:bg-gray-50 ring-1 ring-inset ring-gray-300">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
@@ -51,7 +56,7 @@
             </div>
 
             <!-- Card body: analysis -->
-            <div v-if="dashboard.latest_analysis" class="pb-3">
+            <div v-if="dashboard.latest_analysis">
               <AnalysisIssue v-if="dashboard.latest_analysis.issue" :issue="dashboard.latest_analysis.issue"/>
               <AnalysisExcerpt v-else :analysis="dashboard.latest_analysis"/>
 
