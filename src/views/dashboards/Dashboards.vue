099c89004957be3aed334e98f7f02e4ff3c4c9c9
Ryan Harmon <ryan@bloomcu.com>
2024-09-04 14:43:00 -0600

    Fix deleting dashboard trigger router

--- src/views/dashboards/Dashboards.vue
+++ src/views/dashboards/Dashboards.vue
@@ -125,9 +125,9 @@
       @change="handleDragEvent($event)"
       class="flex flex-col gap-4"
     >
-      <RouterLink
+      <div
         v-for="dashboard in sortedDashboards" 
-        :to="{name: 'dashboard', params: {organization: dashboard.organization.slug, dashboard: dashboard.id}}"
+        @click="router.push({name: 'dashboard', params: {organization: dashboard.organization.slug, dashboard: dashboard.id}})"
         class="group relative flex flex-col cursor-pointer overflow-hidden rounded-lg shadow-sm border bg-white hover:shadow-md"
       >          
         <div class="flex flex-col space-y-4 w-full px-4 py-4">
@@ -149,7 +149,7 @@
               <span class="text-sm text-gray-500 pr-3">Modified {{ moment(dashboard.updated_at).fromNow() }}</span>
 
               <!-- Delete -->
-              <button @click.stop="destroyDashboard(dashboard.id)" class="cursor-pointer font-medium rounded-md p-1.5 text-sm text-gray-400 bg-white hover:bg-gray-50 ring-1 ring-inset ring-gray-300">
+              <button @click.stop="destroyDashboard(dashboard.id)" class="cursor-pointer font-medium rounded-md p-1.5 text-sm text-gray-400 bg-white hover:bg-gray-200 ring-1 ring-inset ring-gray-300">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                   <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
@@ -179,7 +179,7 @@
           <!-- Issue -->
           <AnalysisIssue v-else-if="dashboard.issue" :issue="dashboard.issue"/>
         </div>
-      </RouterLink>
+      </div>
     </VueDraggableNext>
 
     <!-- State: Loading -->
