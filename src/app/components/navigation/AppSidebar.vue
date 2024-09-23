87ebde71b9b12827072b849dec7013389f3a2cb2
Ryan Harmon <ryan@bloomcu.com>
2024-09-23 12:28:39 -0600

    Fix welcome screen

--- src/app/components/navigation/AppSidebar.vue
+++ src/app/components/navigation/AppSidebar.vue
@@ -113,7 +113,7 @@
             </li>
 
             <!-- Onboarding -->
-            <!-- <li v-if="!organizationStore.organization.onboarding.hideOnboarding" class="border-b pb-3">
+            <li v-if="!organizationStore.organization.onboarding.hideOnboarding" class="border-b pb-3 mb-3">
               <ul role="list" class="-mx-2 space-y-1">
                 <li>
                   <RouterLink :to="{name: 'welcome'}" class="group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-medium text-gray-700 hover:text-indigo-600 hover:bg-gray-50">
@@ -122,7 +122,7 @@
                   </RouterLink>
                 </li>
               </ul>
-            </li> -->
+            </li>
             
             <!-- Main menu -->
             <li>
