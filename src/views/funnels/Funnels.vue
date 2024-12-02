5c9a7974fef830f26f633f42419a35734a298d26
Ryan Harmon <ryan@bloomcu.com>
2024-12-02 14:43:46 -0700

    Fix check for connections

--- src/views/funnels/Funnels.vue
+++ src/views/funnels/Funnels.vue
@@ -2,7 +2,7 @@
   <LayoutWithSidebar>
     <template #overlay>
       <!-- Force GA connection first -->
-      <div v-if="organizationStore.organization.onboarding['connect-google-analytics'] !== 'complete'"  class="fixed h-full w-full bg-gray-white bg-opacity-50 backdrop-blur-sm flex justify-center z-50">
+      <div v-if="organizationStore.organization && organizationStore.organization.connections_count == 0"  class="fixed h-full w-full bg-gray-white bg-opacity-50 backdrop-blur-sm flex justify-center z-50">
         <div class="-ml-60 flex flex-col text-center items-center justify-center">
           <svg class="w-10 h-10 mb-8" viewBox="-14 0 284 284" preserveAspectRatio="xMidYMid"><path d="M256.003 247.933a35.224 35.224 0 0 1-39.376 35.161c-18.044-2.67-31.266-18.371-30.826-36.606V36.845C185.365 18.591 198.62 2.881 216.687.24A35.221 35.221 0 0 1 256.003 35.4v212.533Z" fill="#F9AB00"/><path d="M35.101 213.193c19.386 0 35.101 15.716 35.101 35.101 0 19.386-15.715 35.101-35.101 35.101S0 267.68 0 248.295c0-19.386 15.715-35.102 35.101-35.102Zm92.358-106.387c-19.477 1.068-34.59 17.406-34.137 36.908v94.285c0 25.588 11.259 41.122 27.755 44.433a35.161 35.161 0 0 0 42.146-34.56V142.089a35.222 35.222 0 0 0-35.764-35.282Z" fill="#E37400"/></svg>
           <h1 class="mb-2 text-3xl font-medium text-gray-900">Connect Google Analytics</h1>
