b143f2346a3e2a9564f0f6cea7de4a330c309aa9
Ryan Harmon <ryan@bloomcu.com>
2024-12-02 14:00:18 -0700

    Add onboarding messages to welcome screen

--- src/views/welcome/Welcome.vue
+++ src/views/welcome/Welcome.vue
@@ -21,70 +21,82 @@
       </div>
 
       <!-- Welcome -->
-      <div v-else class="mb-8">
-        <h2 class="mb-2 text-3xl font-medium text-gray-900">Welcome!</h2>
-        <p class="text-lg text-gray-700">Let's get set up.</p>
+      <div v-else-if="organizationStore.organization.funnels_count == 0" class="rounded-lg border border-violet-400 bg-violet-50 p-6 mb-8">
+        <div class="flex items-center gap-4 mb-2">
+          <div class="w-6 h-6 border-2 border-t-transparent border-violet-600 rounded-full animate-spin"></div>
+          <h2 class="text-2xl font-medium text-violet-600">Funnels in progress</h2>
+        </div>
+        <p class="text-lg text-gray-700">MetriFi is working on building funnels for you. We’ll notify your email when we’re done.</p>
       </div>
-
+      
       <!-- Step tabs -->
-      <AppCard v-if="organizationStore.organization" padding="none" class="flex mb-6">
-        <div class="w-1/3 p-3 border-r">
-          <ul role="list">
-            <li 
-              v-for="(step, index) in steps" 
-              :key="step.id" 
-              :class="step.current ? 'bg-violet-50 hover:bg-violet-100' : 'bg-white hover:bg-gray-50'" 
-              class="relative flex space-x-3 rounded-lg p-3 cursor-pointer"
-              @click="selectStep(step.id)"
-            >
-              <div v-if="organizationStore.organization.onboarding[step.id] === 'complete'" class="h-8 w-8 rounded-full flex items-center justify-center bg-emerald-500 text-white">
-                <CheckIcon class="h-5 w-5" aria-hidden="true" />
-              </div>
-              <div v-else class="h-8 w-8 rounded-full flex items-center justify-center bg-white border-2 border-gray-300 text-violet-600">
-                <span class="text-violet-600">{{ index + 1 }}</span>
-              </div>
-              <div class="flex items-center min-w-0 font-medium">
-                <p v-if="step.current" class="text-gray-900">{{ step.title }}</p>
-                <p v-else-if="organizationStore.organization.onboarding[step.id] === 'complete'" class="text-gray-400 line-through">{{ step.title }}</p>
-                <p v-else class="text-gray-500">{{ step.title }}</p>
-              </div>
-            </li>
-          </ul>
+      <AppCard v-if="organizationStore.organization" padding="none" class="">
+        <!-- Welcome message -->
+        <div class="p-6 border-b">
+          <h3 v-if="organizationStore.organization.funnels_count == 0" class="text-2xl mb-1 font-medium text-gray-900">In the meantime</h3>
+          <h3 v-else class="text-2xl mb-1 font-medium text-gray-900">Finish Google Analytics setup</h3>
+          <p class="text-lg text-gray-500">Make sure google analytics is configured correctly so we can get all the data we need.</p>
         </div>
 
-        <!-- Content -->
-        <div v-if="currentStep" class="w-2/3 flex items-center px-3 py-10">
-          <div v-if="currentStep.id === 'connect-google-analytics'" class="px-12">
-            <svg class="w-10 h-10 mb-8" viewBox="-14 0 284 284" preserveAspectRatio="xMidYMid"><path d="M256.003 247.933a35.224 35.224 0 0 1-39.376 35.161c-18.044-2.67-31.266-18.371-30.826-36.606V36.845C185.365 18.591 198.62 2.881 216.687.24A35.221 35.221 0 0 1 256.003 35.4v212.533Z" fill="#F9AB00"/><path d="M35.101 213.193c19.386 0 35.101 15.716 35.101 35.101 0 19.386-15.715 35.101-35.101 35.101S0 267.68 0 248.295c0-19.386 15.715-35.102 35.101-35.102Zm92.358-106.387c-19.477 1.068-34.59 17.406-34.137 36.908v94.285c0 25.588 11.259 41.122 27.755 44.433a35.161 35.161 0 0 0 42.146-34.56V142.089a35.222 35.222 0 0 0-35.764-35.282Z" fill="#E37400"/></svg>
-            <h2 class="mb-2 text-2xl font-medium text-gray-900">{{ currentStep.title }}</h2>
-            <div v-html="currentStep.content" class="mb-5 text-lg text-gray-700"></div>
-            <AppButton v-if="organizationStore.organization.onboarding[currentStep.id] !== 'complete'" @click="currentStep.action">{{ currentStep.cta }}</AppButton>
+        <div class="flex mb-6">
+          <div class="w-1/3 p-3 border-r">
+            <ul role="list">
+              <li 
+                v-for="(step, index) in steps" 
+                :key="step.id" 
+                :class="step.current ? 'bg-violet-50 hover:bg-violet-100' : 'bg-white hover:bg-gray-50'" 
+                class="relative flex space-x-3 rounded-lg p-3 cursor-pointer"
+                @click="selectStep(step.id)"
+              >
+                <div v-if="organizationStore.organization.onboarding[step.id] === 'complete'" class="h-8 w-8 rounded-full flex items-center justify-center bg-emerald-500 text-white">
+                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                </div>
+                <div v-else class="h-8 w-8 rounded-full flex items-center justify-center bg-white border-2 border-gray-300 text-violet-600">
+                  <span class="text-violet-600">{{ index + 1 }}</span>
+                </div>
+                <div class="flex items-center min-w-0 font-medium">
+                  <p v-if="step.current" class="text-gray-900">{{ step.title }}</p>
+                  <p v-else-if="organizationStore.organization.onboarding[step.id] === 'complete'" class="text-gray-400 line-through">{{ step.title }}</p>
+                  <p v-else class="text-gray-500">{{ step.title }}</p>
+                </div>
+              </li>
+            </ul>
           </div>
 
-          <div v-else class="px-12">
-            <div v-if="currentStep.video" class="mb-8 aspect-video">
-              <video v-if="currentStep.id === 'enable-enhanced-measurement'" class="h-full w-full rounded-lg" controls>
-                <source src="/video/enable-enhanced-measurement.mp4" type="video/mp4">
-              </video>
-              <video v-if="currentStep.id === 'extend-data-retention-period'" class="h-full w-full rounded-lg" controls>
-                <source src="/video/extend-data-retention-period.mp4" type="video/mp4">
-              </video>
-              <video v-if="currentStep.id === 'setup-cross-domain-tracking'" class="h-full w-full rounded-lg" controls>
-                <source src="/video/setup-cross-domain-tracking.mp4" type="video/mp4">
-              </video>
-              <video v-if="currentStep.id === 'add-custom-dimensions'" class="h-full w-full rounded-lg" controls>
-                <source src="/video/add-custom-dimensions.mp4" type="video/mp4">
-              </video>
-              <video v-if="currentStep.id === 'filter-out-internal-traffic'" class="h-full w-full rounded-lg" controls>
-                <source src="/video/filter-out-internal-traffic.mp4" type="video/mp4">
-              </video>
+          <!-- Content -->
+          <div v-if="currentStep" class="w-2/3 flex items-center px-3 py-10">
+            <div v-if="currentStep.id === 'connect-google-analytics'" class="px-12">
+              <svg class="w-10 h-10 mb-8" viewBox="-14 0 284 284" preserveAspectRatio="xMidYMid"><path d="M256.003 247.933a35.224 35.224 0 0 1-39.376 35.161c-18.044-2.67-31.266-18.371-30.826-36.606V36.845C185.365 18.591 198.62 2.881 216.687.24A35.221 35.221 0 0 1 256.003 35.4v212.533Z" fill="#F9AB00"/><path d="M35.101 213.193c19.386 0 35.101 15.716 35.101 35.101 0 19.386-15.715 35.101-35.101 35.101S0 267.68 0 248.295c0-19.386 15.715-35.102 35.101-35.102Zm92.358-106.387c-19.477 1.068-34.59 17.406-34.137 36.908v94.285c0 25.588 11.259 41.122 27.755 44.433a35.161 35.161 0 0 0 42.146-34.56V142.089a35.222 35.222 0 0 0-35.764-35.282Z" fill="#E37400"/></svg>
+              <h2 class="mb-2 text-2xl font-medium text-gray-900">{{ currentStep.title }}</h2>
+              <div v-html="currentStep.content" class="mb-5 text-lg text-gray-700"></div>
+              <AppButton v-if="organizationStore.organization.onboarding[currentStep.id] !== 'complete'" @click="currentStep.action">{{ currentStep.cta }}</AppButton>
             </div>
 
-            <h2 class="mb-2 text-2xl font-medium text-gray-900">{{ currentStep.title }}</h2>
-            <div v-html="currentStep.content" class="mb-5 text-lg text-gray-700"></div>
+            <div v-else class="px-12">
+              <div v-if="currentStep.video" class="mb-8 aspect-video">
+                <video v-if="currentStep.id === 'enable-enhanced-measurement'" class="h-full w-full rounded-lg" controls>
+                  <source src="/video/enable-enhanced-measurement.mp4" type="video/mp4">
+                </video>
+                <video v-if="currentStep.id === 'extend-data-retention-period'" class="h-full w-full rounded-lg" controls>
+                  <source src="/video/extend-data-retention-period.mp4" type="video/mp4">
+                </video>
+                <video v-if="currentStep.id === 'setup-cross-domain-tracking'" class="h-full w-full rounded-lg" controls>
+                  <source src="/video/setup-cross-domain-tracking.mp4" type="video/mp4">
+                </video>
+                <video v-if="currentStep.id === 'add-custom-dimensions'" class="h-full w-full rounded-lg" controls>
+                  <source src="/video/add-custom-dimensions.mp4" type="video/mp4">
+                </video>
+                <video v-if="currentStep.id === 'filter-out-internal-traffic'" class="h-full w-full rounded-lg" controls>
+                  <source src="/video/filter-out-internal-traffic.mp4" type="video/mp4">
+                </video>
+              </div>
 
-            <AppButton v-if="organizationStore.organization.onboarding[currentStep.id] === 'complete'" @click="markStepIncomplete(currentStep.id)" variant="tertiary">Mark as incomplete</AppButton>
-            <AppButton v-else @click="currentStep.action">{{ currentStep.cta }}</AppButton>
+              <h2 class="mb-2 text-2xl font-medium text-gray-900">{{ currentStep.title }}</h2>
+              <div v-html="currentStep.content" class="mb-5 text-lg text-gray-700"></div>
+
+              <AppButton v-if="organizationStore.organization.onboarding[currentStep.id] === 'complete'" @click="markStepIncomplete(currentStep.id)" variant="tertiary">Mark as incomplete</AppButton>
+              <AppButton v-else @click="currentStep.action">{{ currentStep.cta }}</AppButton>
+            </div>
           </div>
         </div>
       </AppCard>
