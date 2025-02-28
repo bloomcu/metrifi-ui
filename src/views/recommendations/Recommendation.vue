9a6931b853bdff81584fd6b8030b9b59d1a6a35e
Ryan Harmon <ryan@bloomcu.com>
2025-02-27 18:27:31 -0700

    Persist prototype updates

--- src/views/recommendations/Recommendation.vue
+++ src/views/recommendations/Recommendation.vue
@@ -475,12 +475,13 @@ const sendMessage = async () => {
     if (lastValidHtml !== recommendationStore.recommendation.prototype) {
       recommendationStore.recommendation.prototype = lastValidHtml
     }
-    // await recommendationStore.update(
-    //   route.params.organization,
-    //   route.params.dashboard,
-    //   route.params.recommendation,
-    //   { prototype: lastValidHtml }
-    // )
+    
+    await recommendationStore.update(
+      route.params.organization,
+      route.params.dashboard,
+      route.params.recommendation,
+      { prototype: lastValidHtml }
+    )
 
   } catch (error) {
     console.error('Error streaming from Grok:', error)
