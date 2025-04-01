15792f980b995227330270958b3c2ab4a0c19147
Ryan Harmon <ryan@bloomcu.com>
2025-04-01 14:39:05 -0600

    Add block regenerate api endpoint

--- src/domain/blocks/api/blocksApi.js
+++ src/domain/blocks/api/blocksApi.js
@@ -3,7 +3,11 @@ import { httpClient as HttpClient } from '@/app/api/base/httpClient'
 const blocksApi = {    
     async update(organizationSlug, blockId, params) {
       return await HttpClient.put(`/${organizationSlug}/blocks/${blockId}`, params)
-    }
+    },
+
+    async regenerate(organizationSlug, blockId, params) {
+        return await HttpClient.put(`/${organizationSlug}/blocks/${blockId}/regenerate`)
+      }
 }
 
 export { blocksApi }
