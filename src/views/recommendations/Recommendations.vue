adba2d3b81bb410e69ab3122a189b5f90d54bc7d
Ryan Harmon <ryan@bloomcu.com>
2025-04-15 17:22:55 -0600

    bump

--- src/views/recommendations/Recommendations.vue
+++ src/views/recommendations/Recommendations.vue
@@ -42,7 +42,7 @@
                 <!-- Versions -->
                 <!-- <span class="text-sm text-gray-500 border-r border-gray-300 pr-3">6 versions</span> -->
 
-               <!-- Status -->
+                <!-- Status -->
                 <!-- TODO: Move this into a component -->
                 <span v-if="recommendation.status == 'done'" class="text-emerald-600 text-sm">Done</span>
                 <span v-if="recommendation.status == 'draft'" class="bg-gray-100 border border-gray-300 py-1 px-2.5 rounded-full text-gray-600 text-sm">Draft</span>
@@ -50,6 +50,7 @@
                 <span v-if="recommendationStore.isFailed(recommendation.status)" class="text-red-600 text-sm">Failed</span>
               
                 <!-- Copy -->
+                <!-- TODO: Enable this -->
                 <!-- <button @click.stop="replicateDashboard(dashboard.id)" class="cursor-pointer font-medium rounded-md p-1.5 text-sm text-gray-400 bg-white hover:bg-gray-200 ring-1 ring-inset ring-gray-300">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 8.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v8.25A2.25 2.25 0 0 0 6 16.5h2.25m8.25-8.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-7.5A2.25 2.25 0 0 1 8.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 0 0-2.25 2.25v6" />
@@ -57,6 +58,7 @@
                 </button> -->
 
                 <!-- Delete -->
+                <!-- TODO: Enable this -->
                 <!-- <button @click.stop="destroyDashboard(dashboard.id)" class="cursor-pointer font-medium rounded-md p-1.5 text-sm text-gray-400 bg-white hover:bg-gray-200 ring-1 ring-inset ring-gray-300">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
@@ -172,7 +174,7 @@ const createFromScratch = () => {
     step_index: null,
     metadata: null
   }).then(() => {
-    router.push({ name: 'recommendation', params: { organization: route.params.organization, recommendation: recommendationStore.recommendation.id } })
+    router.push({ name: 'recommendationEdit', params: { organization: route.params.organization, recommendation: recommendationStore.recommendation.id } })
   })
 }
 
