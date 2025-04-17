476c0fdd9cda738c1b3d603d5e0c7155218ee1b6
Ryan Harmon <ryan@bloomcu.com>
2025-04-17 13:07:43 -0600

    bump

--- src/domain/blocks/api/blocksApi.js
+++ src/domain/blocks/api/blocksApi.js
@@ -7,7 +7,19 @@ const blocksApi = {
 
     async regenerate(organizationSlug, blockId, params) {
         return await HttpClient.put(`/${organizationSlug}/blocks/${blockId}/regenerate`)
-      }
+    },
+
+    async getVersions(organizationSlug, blockId) {
+        return await HttpClient.get(`/${organizationSlug}/blocks/${blockId}/versions`)
+    },
+
+    async revert(organizationSlug, blockId) {
+        return await HttpClient.put(`/${organizationSlug}/blocks/${blockId}/versions/revert`)
+    },
+
+    async advance(organizationSlug, blockId) {
+        return await HttpClient.put(`/${organizationSlug}/blocks/${blockId}/versions/advance`)
+    },
 }
 
 export { blocksApi }
