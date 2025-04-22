3fedee2f5604ce0bc8f0515ba06032cdbd51b055
Ryan Harmon <ryan@bloomcu.com>
2025-04-22 16:39:44 -0600

    Can add new blocks in specific order

--- src/views/recommendations/components/Prototype.vue
+++ src/views/recommendations/components/Prototype.vue
@@ -25,24 +25,30 @@
               </button>
             </div>
 
-            <div v-if="!block.status" class="m-2 border rounded-md p-3 bg-gray-200 animate-pulse">
-                
+            <!-- Block is empty -->
+            <div v-if="!block.status" class="m-2 border rounded-md p-2 bg-gray-200">
+                <div class="flex items-center gap-2">
+                  <AppInput
+                    v-model="block.outline"
+                    @keydown.enter="generateBlock(block)"
+                    placeholder="Describe the block you want to design"
+                    class="flex-1 resize-none"
+                  />
+                  <AppButton @click="generateBlock(block)" :disabled="!block.outline">
+                    Submit
+                  </AppButton>
+                </div>
             </div>
 
-            <div v-if="block.status === 'building'" class="m-2 border rounded-md p-3 bg-gray-100 animate-pulse">
+            <!-- Block is being generated -->
+            <div v-if="block.status === 'generating'" class="m-2 border rounded-md p-3 bg-gray-100 animate-pulse">
                 <div class="flex items-center gap-1 text-violet-600">
                     <svg aria-hidden="true" role="status" class="position w-4 h-4 mr-2 animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#FFFFFF" fill-opacity="0"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/></svg>
                     <span class="text-sm">{{ block.type ? 'Building ' + block.type : 'Building block' }}</span>
                 </div>
             </div>
 
-            <div v-if="block.status === 'regenerating'" class="m-2 border rounded-md p-3 bg-gray-200 animate-pulse">
-                <div class="flex items-center gap-1 text-violet-600">
-                    <svg aria-hidden="true" role="status" class="position w-4 h-4 mr-2 animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#FFFFFF" fill-opacity="0"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/></svg>
-                    <span class="text-sm">Designing block</span>
-                </div>
-            </div>
-
+            <!-- Block has been generated -->
             <div v-else-if="block.html" class="relative">
                 <div v-if="recommendationStore.recommendation.status === 'done'">
                     <div class="absolute z-[9999] top-2 right-2 flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
@@ -51,8 +57,8 @@
                             <button 
                                 v-if="canRevertBack(block)"
                                 @click="revertToPreviousVersion(block)" 
-                                class="p-0.5 -ml-2 flex items-center rounded-md gap-1 disabled:opacity-50 disabled:cursor-not-allowed"
                                 :class="canRevertBack(block) ? 'bg-slate-600 hover:bg-slate-700' : ''"
+                                class="p-0.5 -ml-2 flex items-center rounded-md gap-1 disabled:opacity-50 disabled:cursor-not-allowed"
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
@@ -72,20 +78,12 @@
                         </div>
                         
                         <!-- Regenerate button -->
-                        <button 
-                            v-if="block.outline" 
-                            @click.stop="regenerateBlock(block)" 
-                            class="bg-[#884DFF] hover:bg-[#6E3ECE] text-white px-3 py-1 rounded-md text-sm"
-                        >
+                        <button v-if="block.outline" @click.stop="regenerateBlock(block)" class="bg-[#884DFF] hover:bg-[#6E3ECE] text-white px-3 py-1 rounded-md text-sm">
                             Regenerate
                         </button>
 
                         <!-- Disabled regenerate button -->
-                        <button 
-                            v-if="!block.outline" 
-                            @click.stop="regenerateBlock(block)" 
-                            class="bg-[#884DFF]/50 hover:bg-[#6E3ECE]/50 text-white px-3 py-1 rounded-md text-sm"
-                        >
+                        <button v-if="!block.outline" @click.stop="regenerateBlock(block)" class="bg-[#884DFF]/50 hover:bg-[#6E3ECE]/50 text-white px-3 py-1 rounded-md text-sm">
                             Block must have an outline to be regenerated
                         </button>
                     </div>
@@ -111,54 +109,68 @@
 import { useRecommendationStore } from '@/domain/recommendations/store/useRecommendationStore'
 import { useRoute } from 'vue-router'
 import { blocksApi } from '@/domain/blocks/api/blocksApi'
+import { ref } from 'vue'
 
 const route = useRoute()
 const recommendationStore = useRecommendationStore()
-const emit = defineEmits(['regenerate-block', 'add-block'])
+const emit = defineEmits(['fetch-recommendation'])
 
 const selectBlock = (block) => {
-  recommendationStore.selectedBlock = block
+    if (block.html) {
+        recommendationStore.selectedBlock = block
+    } else {
+        recommendationStore.selectedBlock = null
+    }
 }
 
-const addNewBlock = (order) => {
-  emit('add-block', recommendationStore.recommendation.latest_page.id, order)
-}
-
-const regenerateBlock = async (block) => {
-  // Set block status to regenerating
-  block.status = 'regenerating'
-  
+const addNewBlock = async (order) => {
   try {
-    await blocksApi.regenerate(route.params.organization, block.id)
+    await blocksApi.store(
+      route.params.organization,
+      { 
+        page_id: recommendationStore.recommendation.latest_page.id,
+        order: order 
+      }
+    )
     
-    // Emit event to parent component
-    emit('regenerate-block')
+    // Fetch the updated recommendation with the new block
+    emit('fetch-recommendation')
   } catch (error) {
-    console.error('Error regenerating block:', error)
-    // Reset status if there was an error
-    block.status = null
+    console.error('Error adding new block:', error)
   }
 }
 
 const generateBlock = async (block) => {
-  // Update the block locally
   block.status = 'generating'
-  block.prompt = blockOutline.value
   
   try {
     // Update the block in db and regenerate
-    await blocksApi.update(route.params.organization, block.id, { outline: blockOutline.value })
+    await blocksApi.update(route.params.organization, block.id, { outline: block.outline })
         .then(() => {
             blocksApi.regenerate(route.params.organization, block.id)
         })
     
-    // Emit event to parent component
-    emit('regenerate-block')
+    emit('fetch-recommendation')
   } catch (error) {
     // Reset status if there was an error
     block.status = null
 
     console.error('Error generating block:', error)
+  }
+}
+
+const regenerateBlock = async (block) => {
+  // Set block status to regenerating
+  block.status = 'generating'
+  
+  try {
+    await blocksApi.regenerate(route.params.organization, block.id)
+    
+    emit('fetch-recommendation')
+  } catch (error) {
+    console.error('Error regenerating block:', error)
+    // Reset status if there was an error
+    block.status = null
   }
 }
 
